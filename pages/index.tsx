ad0a8b5850c51d620d05da13fb37ac6e05d21ac7
shawn d'souza <shawnkdsouza@outlook.com>
2022-02-06 20:56:15 -0500

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,70 +1,98 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import ReactTwitchEmbedVideo from "react-twitch-embed-video"
+import { useState } from "react";
+import CountUp from 'react-countup';
+
+import { useMoralis } from "react-moralis";
+import { truncateAddress } from "../utils/shortAddy";
+
 
 export default function Home() {
+
+  
+
+  const [playing, setPlaying] = useState(false);
+
+  const {
+    authenticate,
+    isAuthenticated,
+    logout,
+    user,
+    isWeb3Enabled,
+    enableWeb3,
+  } = useMoralis();
+
+
+
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div >
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <div className="flex p-4">
+        <h1 className="text-2xl font-bold p-3ß">sake</h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="flex flex-auto gap-4 p-3 justify-center">
+          <a>explore</a>
+          <a>my tokens</a>
+          <a>trade</a>
         </div>
+
+        <div>
+          {!isAuthenticated ?
+          <button 
+          className="text-md font-bold rounded-md bg-indigo-500 p-3 text-white"
+          onClick={() => {
+            authenticate({provider: "metamask"})
+          } }
+          
+          >log in</button>
+          :
+          <div className="flex grid-row gap-2">
+            <h1 className="py-3  ">{truncateAddress(user.get("ethAddress"))}</h1>
+            <button 
+            className="text-md font-bold rounded-md bg-indigo-500 p-3 text-white"
+            onClick={() => {
+              logout()
+            } }
+            
+            >log out
+            </button>
+          </div>
+
+        }
+        </div>
+
+
+      </div>
+
+      
+
+      
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+
+      
+      {/* <ReactTwitchEmbedVideo className="p-4" channel="nickmercs" layout="video-with-chat" height="480" width="1080" onPlay={() => setPlaying } /> */}
+        
+      {/* <ReactTwitchEmbedVideo className="p-4" channel="nickmercs" layout="video-with-chat" height="480" width="1080" onPlay={() => setPlaying(true)} />
+      { playing ? <div className="flex p-4 gap-2 py-4"><h1 className="font-bold">superfluid token stream</h1><CountUp end={100000} duration={500000} decimals={3}/> 
+      <h1>tokens</h1></div> : <h1>count</h1>} */}
+
+      <ReactTwitchEmbedVideo className="p-4" channel="nickmercs" layout="video-with-chat" height="480" width="1080" onPlay={() => setPlaying(true) } />
+      { playing ? <div className="flex p-4 gap-2 py-4"><h1 className="font-bold">superfluid token stream</h1><CountUp end={100000} duration={500000} decimals={3}/> 
+      <h1>tokens</h1></div> : <h1>count</h1>}
+      
+      
+        
+
+        
       </main>
+      </div>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
